02c15f2e27c0c942bd367eeef113d055500c2f57
Mark Gandolfo <mark@gandolfo.com.au>
2024-06-23 18:50:50 +1000

    handle exit 0

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,31 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-
     loop {
-        // Remove the newline character from the input
-        input = input.trim().to_string();
-
-        handle_input(&input);
-
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        input.clear();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
+
+        handle_input(&input);
+        input.clear();
     }
 }
 
 fn handle_input(input: &str) {
-    match input.trim() {
-        "exit" => {
+    let input: Vec<&str> = input.split_whitespace().collect();
+
+    match input.as_slice() {
+        ["exit", code] => {
+            let code: i32 = code.parse().unwrap_or(0);
             println!("Goodbye!");
-            std::process::exit(0);
+            std::process::exit(code);
         }
         command => {
-            println!("{}: command not found", command);
+            println!("{}: command not found", command.join(" "));
         }
     }
 }
